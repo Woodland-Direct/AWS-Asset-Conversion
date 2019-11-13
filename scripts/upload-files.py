088bafc594a473047cabb5768a515f15ff76b605
Erica Perry <erica.perry@woodlanddirect.com>
2019-11-13 13:13:57 -0500

    [aws-upload][sx]add shebang at top of file

--- scripts/upload-files.py
+++ scripts/upload-files.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+"""Read, edit, and upload CSV file to AWS
+
+Usage: 
+
+    python3 upload-files.py <URL>
+"""
+
 def read_file(file_path):
     try:
         file = open(file_path, "r")
@@ -6,3 +14,15 @@ def read_file(file_path):
         file.close()
     except Exception as e:
         print("Could not read file", e)
+
+# def edit_file():
+#     try:
+#         file = open("students.txt", "a")
+#         file.write("filler")
+#         file.close()
+#     except Exception:
+#         print("Could not edit file")
+
+
+# def upload_file():
+#     print('upload')
