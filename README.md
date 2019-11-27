c0e9e99e1c3f9deac49c7f69a215bd8d9338b952
Erica Perry <erica.perry@woodlanddirect.com>
2019-11-27 15:30:02 -0500

    aws-upload: upload functionality working

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AWS-Asset-Conversion
 A collection of scripts to move our assets from Netsuite to AWS and then some.
+
+install python 3
+install boto3
+install awscli
+configure aws
+run it
--- scripts/upload-files.py
+++ scripts/upload-files.py
@@ -7,14 +7,16 @@ Usage:
 """
 
 import boto3
+import urllib
 
 client = boto3.client('s3', region_name='us-west-2')
 
-def read_file(file_path):
+def read_file(file_path, bucket):
     """Opens files and prints every line.
 
     Args:
         file_path: CSV file name in the root directory of folder.
+        bucket: String to AWS bucket
 
     Returns:
         A list of strings containing all rows, with columns seperated by a comma.
@@ -28,33 +30,18 @@ def read_file(file_path):
         print("Could not read file", e)
 
 
-def edit_file(file_path, add):
-    """Adds a new line to your file
-
-    Args:
-        file_path: CSV file name in the root directory of folder.
-        add: string to be written on the file, with columns seperated by commas
-    """
-    try:
-        file = open(file_path, "a")
-        file.write("\n" + add)
-        file.close()
-    except Exception as e:
-        print("Could not edit file", e)
-
-
-def upload_file(item):
+def upload_file(item, bucket):
     """Upload file to S3 Bucket
 
     Args:
         item: Array containing img url and item name
+        bucket: String to AWS bucket
     """
     try:
-        fileName = item[0]
+        fileName = 'images/' + item[0]
         image = item[1].strip('\n')
-        # fileName = 'doge'
-        # image = 'images/69o34n3l88141.jpg'
-        print(fileName, image)
-        # client.upload_file(image, 'reviewimages.woodlanddirect.com', fileName)
+        imgPath = fileName + '.jpg'
+        urllib.request.urlretrieve(image, imgPath)
+        client.upload_file(imgPath, bucket, fileName)
     except Exception as e:
         print("Could not upload file", e)
