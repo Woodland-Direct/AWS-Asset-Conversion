b54af0835751584e26e2051b7efcf90857b7de09
Erica Perry <erica.perry@woodlanddirect.com>
2020-04-24 12:35:42 -0400

    add, remove: func back in and removal of new line on file ext, moved extra imports

--- scripts/upload-files.py
+++ scripts/upload-files.py
@@ -2,18 +2,15 @@
 """Read, edit, and upload CSV file to AWS
 
 Usage:
-
     python3 -i scripts/upload-files.py
 """
 
 import boto3
-import urllib
 import os
 import requests
 import re
-from PIL import Image
-from io import BytesIO
-import wget
+
+
 
 client = boto3.client("s3", region_name="us-west-2")
 
@@ -34,8 +31,7 @@ def read_file(file_path, bucket):
             line = item.split(",")
             image = line[1].strip("\n")
             item_id = line[0]
-            #test = wget.download(url)
-            file_name = download_image(image)
+            file_name = download_image(image, item_id, line[5])
             #upload_file_to_AWS(file_name, bucket)
             ns_name = line[5]
             ns_url = 'https://'+bucket+'/'+file_name
@@ -46,23 +42,41 @@ def read_file(file_path, bucket):
     except Exception as e:
         print("Could not read file", e)
 
-def download_image(download_url):
+def download_image(download_url, item_id, file_identifier):
     """Download file locally from URL
 
     Args:
         download_url: String, url to image you want to download
+        item_id: String, id of item for file name
+        file_identifier: String, what image will be used for
     """
     try:
         url = download_url
         r = requests.get(url)
         d = r.headers['content-disposition']
         fname = re.findall("filename=(.+)", d)[0]
-        with open('images/' + fname, 'wb') as outfile:
+        identifier = strip_and_replace(file_identifier, ' ', '-')
+        ext = get_file_ext(fname)
+        file_path = "".join(["images/", item_id, '-', identifier, ext])
+        with open(file_path, 'wb') as outfile:
             outfile.write(r.content)
-        return fname
+        return file_path
     except Exception as e:
         print("Could not download file", e)
 
+def strip_and_replace(str, text_to_replace, replace_with):	
+    """	
+    send a str and a replacement for it	
+    """	
+    return str.replace(text_to_replace, replace_with).strip("\n")
+
+
+def get_file_ext(url):	
+    """Gets the file extension from a url	
+    Args:	
+        url: url to image	
+    """	
+    return "." + url.split(".")[-1]
 
 def upload_file_to_AWS(file_name, bucket):
     """Upload file to S3 Bucket
